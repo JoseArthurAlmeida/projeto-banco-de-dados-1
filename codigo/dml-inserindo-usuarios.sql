e68628b89a78f32b092fc6b399746de5b4302fed
José Arthur <ja1364091@gmail.com>
2025-12-07 19:27:15 -0300

    Adicionando DML dos usuários

---
+++ codigo/dml-inserindo-usuarios.sql
@@ -0,0 +1,8 @@
+insert into usuario (id, nome, email, senha)
+values (1, 'Administrador', 'admin@sistema.com', 'admin123'),
+       (2, 'Arthur', 'arthur@email.com', '123'),
+       (3, 'José', 'jose@email.com', '124'),
+       (4, 'Mariana', 'mariana@email.com', 'senha123'),
+       (5, 'Carlos', 'carlos@email.com', 'senha456'),
+       (6, 'Beatriz', 'beatriz@email.com', 'senha789'),
+       (7, 'Lucas', 'lucas@email.com', 'senha101');
